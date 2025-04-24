59edefa5557d752f018e6402cdc08706e96dd8ab
choyeongju <jjudy1301@naver.com>
2025-04-25 03:59:31 +0900

    [feat] dto

---
+++ src/main/java/org/sopt/post/api/dto/request/PostUpdateDto.java
@@ -0,0 +1,11 @@
+package org.sopt.post.api.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PostUpdateDto(
+        @NotBlank(message = "제목은 필수로 입력되어야 합니다.")
+        @Size(max = 30, message = "제목은 30자 이하여야 합니다.")
+        String newTitle
+) {
+}
---
+++ src/main/java/org/sopt/post/api/dto/response/PostListResponse.java
@@ -0,0 +1,20 @@
+package org.sopt.post.api.dto.response;
+
+import org.sopt.post.core.domain.Post;
+
+import java.util.List;
+
+public record PostListResponse(
+        List<PostDto> postsLists
+) {
+    public record PostDto(
+            int id,
+            String name
+    ) {
+        public static PostDto from(Post post) {
+            return new PostDto(
+                    post.getId(), post.getTitle()
+            );
+        }
+    }
+}
