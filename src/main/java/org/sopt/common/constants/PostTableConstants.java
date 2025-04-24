9f387ad3960a9c8c6508a552640898c6f65f665c
choyeongju <jjudy1301@naver.com>
2025-04-25 04:00:02 +0900

    [feat] 기타

---
+++ src/main/java/org/sopt/common/constants/PostTableConstants.java
@@ -0,0 +1,7 @@
+package org.sopt.common.constants;
+
+public class PostTableConstants {
+    public final static String TABLE_POST = "post";
+    public final static String COLUMN_ID = "id";
+    public final static String COLUMN_TITLE = "title";
+}
