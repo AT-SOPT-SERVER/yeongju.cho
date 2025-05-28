52e17f3fce0400e5f4642ce1d6f3e70dad9c1287
choyeongju <jjudy1301@naver.com>
2025-05-28 10:44:30 +0900

    [feat] Login api 구현

--- src/main/java/org/sopt/user/api/controller/UserController.java
+++ src/main/java/org/sopt/user/api/controller/UserController.java
@@ -1,31 +1,36 @@
 package org.sopt.user.api.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.sopt.auth.core.service.AuthService;
+import org.sopt.auth.api.dto.JwtTokenDto;
 import org.sopt.user.api.service.UserService;
 import org.sopt.user.api.dto.UserSignupDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/auth")
 public class UserController {
 
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+    private final AuthService authService;
 
     @PostMapping("/users/signup")
     public ResponseEntity<Void> signup(
             @RequestBody @Valid final UserSignupDto userSignupDto
-    ){
+    ) {
         long userId = userService.signup(userSignupDto);
         return ResponseEntity.created(URI.create(Long.valueOf(userId).toString())).build();
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<JwtTokenDto> login(
+            @RequestParam final Long userId
+    ){
+        return ResponseEntity.ok(authService.login(userId));
     }
 }
