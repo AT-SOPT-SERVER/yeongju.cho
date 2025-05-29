1d72451bab0b9621fb62258ff45b56df3e814141
choyeongju <jjudy1301@naver.com>
2025-05-29 14:17:45 +0900

    [feat] @UserId 어노테이션 기반으로 컨트롤러 수정

--- src/main/java/org/sopt/post/api/controller/PostController.java
+++ src/main/java/org/sopt/post/api/controller/PostController.java
@@ -1,6 +1,8 @@
 package org.sopt.post.api.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.sopt.common.annotation.UserId;
 import org.sopt.post.api.dto.request.PostCreateDto;
 import org.sopt.post.api.dto.response.PostDetailsResponse;
 import org.sopt.post.api.dto.request.PostUpdateDto;
@@ -15,19 +17,15 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/posts")
 public class PostController {
     private final PostService postService;
     private final UserService userService;
 
-    public PostController(PostService postService, UserService userService) {
-        this.postService = postService;
-        this.userService = userService;
-    }
-
     @PostMapping
     public ResponseEntity<Void> createPost(
-            @RequestHeader("Authorization") final long userId,
+            @UserId final long userId,
             @RequestBody @Valid final PostCreateDto postCreateDto
     ) {
         long postId = postService.createPost(userId, postCreateDto);
@@ -37,8 +35,8 @@ public class PostController {
     // 게시글 상세 조회
     @GetMapping("/{post-id}")
     public ResponseEntity<PostDetailsResponse> getPostDetails (
-            @RequestHeader("Authorization") final long userId,
-            @PathVariable(name = "post-id") final int postId
+            @UserId final long userId,
+            @PathVariable(name = "post-id") final long postId
     ) {
         final Post post = postService.getPostDetails(userId, postId);
         final User user = userService.findById(post.getUserId());
@@ -48,27 +46,30 @@ public class PostController {
     // 게시글 전체 조회
     @GetMapping
     public ResponseEntity<PostListResponse> getPostList(
+            @UserId final long userId,
             @RequestParam(required = false) final String keyword
     ) {
         return ResponseEntity.ok(postService.getAllPosts(keyword));
     }
 
+    // 제목, 내용 수정
     @PutMapping("/{post-id}")
-    public ResponseEntity<Void> updatePostTitle (
-            @RequestHeader("Authorization") final long userId,
-            @PathVariable(name = "post-id") final int postId,
+    public ResponseEntity<Void> updatePost (
+            @UserId final long userId,
+            @PathVariable(name = "post-id") final long postId,
             @RequestBody @Valid final PostUpdateDto postUpdateDto
     ) {
-        postService.updatePostTitle(userId, postId, postUpdateDto);
+        postService.updatePost(userId, postId, postUpdateDto);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{post-id}")
     public ResponseEntity<Void> deletePostById (
-            @RequestHeader("Authorization") final long userId,
-            @PathVariable(name = "post-id") final int postId
+            @UserId final long userId,
+            @PathVariable(name = "post-id") final long postId
     ) {
         postService.deletePost(userId, postId);
         return ResponseEntity.noContent().build();
     }
+
 }
