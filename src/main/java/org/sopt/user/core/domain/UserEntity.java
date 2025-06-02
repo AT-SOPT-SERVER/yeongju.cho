f1a016c8525c97c91edd836b0dd06a24f938c6ca
choyeongju <jjudy1301@naver.com>
2025-06-02 16:05:50 +0900

    [feat] BaseTimeEntity 도입

--- src/main/java/org/sopt/user/core/domain/UserEntity.java
+++ src/main/java/org/sopt/user/core/domain/UserEntity.java
@@ -1,35 +1,54 @@
 package org.sopt.user.core.domain;
 
 import jakarta.persistence.*;
-import org.sopt.common.constants.UserTableConstants;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.sopt.common.domain.BaseTimeEntity;
 
-import static org.sopt.common.constants.UserTableConstants.*;
+import static org.sopt.user.core.domain.UserTableConstants.*;
 
 @Entity
-@Table(name = UserTableConstants.TABLE_USER)
-public class UserEntity {
+@Table(name = TABLE_USER, indexes = {
+        @Index(name = "idx_user_nickname", columnList = "nickname", unique = true)
+})
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserEntity extends BaseTimeEntity {
 
     @Id
     @Column(name = COLUMN_ID)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = NICKNAME)
+    @NotBlank
+    @Column(name = NICKNAME, nullable = false)
     private String nickname;
-
-    protected UserEntity(){
-
-    }
 
     public UserEntity(String nickname) {
         this.nickname = nickname;
     }
 
-    public Long getId(){
-        return this.id;
+    // 신규 저장용
+    public static UserEntity forCreate(User user) {
+        return new UserEntity(user.getNickname());
     }
 
-    public String getNickname(){
-        return this.nickname;
+    // Entity -> Domain
+    public User toDomain() {
+        return User.builder()
+                .id(id)
+                .nickname(nickname)
+                .createdAt(getCreatedAt())
+                .updatedAt(getUpdatedAt())
+                .build();
     }
+
+//    // Domain -> Entity (조회 및 수정)
+//    public static UserEntity fromDomain(User user) {
+//        return new UserEntity(
+//                user.getId(),
+//                user.getNickname());
+//    }
+
 }
