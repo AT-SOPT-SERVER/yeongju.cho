472fce0cd37201c16b5f6145b4af5b8508f96992
choyeongju <jjudy1301@naver.com>
2025-06-12 13:48:20 +0900

    [refac] dto 구조 수정

--- src/main/java/org/sopt/post/api/dto/request/PostUpdateDto.java
+++ src/main/java/org/sopt/post/api/dto/request/PostUpdateDto.java
@@ -4,10 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record PostUpdateDto(
-        @NotBlank(message = "제목은 필수로 입력되어야 합니다.")
         @Size(max = 30, message = "제목은 30자 이하여야 합니다.")
         String newTitle,
         @Size(max = 1000, message = "게시글의 내용은 1000자 이하여야 합니다.")
-        String content
+        String newContent
 ) {
 }
